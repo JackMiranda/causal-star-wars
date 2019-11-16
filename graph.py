f41c1bf9baa15265ac05e039f4e845b6031af271
JackMiranda <19409711+JackMiranda@users.noreply.github.com>
2019-11-16 13:52:53 -0700

    Graph

---
+++ graph.py
@@ -0,0 +1,14 @@
+import json
+
+from networkx.readwrite import json_graph
+import pickle
+from networkx import DiGraph
+
+with open('graph.pickle','rb') as f:
+    dag = pickle.load(f)
+
+for n in dag.nodes:
+    dag.nodes[n]['prob'] = []
+
+with open('graph.json','w') as f:
+    f.write(json.dumps(json_graph.node_link_data(dag)))
